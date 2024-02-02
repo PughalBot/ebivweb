e86bf4e60552460ca552baa433e5df75a5aa1e12
PughalBot <pughal2003@gmail.com>
2024-02-02 15:24:53 +0530

    init website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,111 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ["latin"] });
+const AnimatedLetter = ({ letter, delay }) => (
+  <motion.span
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    exit={{ opacity: 0, y: -20 }}
+    transition={{ duration: 0.5, delay }}
+  >
+    {letter}
+  </motion.span>
+);
 
 export default function Home() {
+  const [showLogo, setShowLogo] = useState(false);
+  const [showMarquee, setShowMarquee] = useState(false);
+  const [currentText, setCurrentText] = useState([]);
+  const texts = ["STAY TUNED!!!", "Subscribe to get notified"];
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const animateText = (text, index = 0, isAppearing = true) => {
+    if (index <= text.length && isAppearing) {
+      setCurrentText([...text.slice(0, index)]);
+      setTimeout(() => animateText(text, index + 1, true), 100);
+    } else if (index >= 0 && !isAppearing) {
+      setCurrentText([...text.slice(0, index)]);
+      setTimeout(() => animateText(text, index - 1, false), 100);
+    } else {
+      if (isAppearing) {
+        setTimeout(() => animateText(text, text.length - 1, false), 3000);
+      } else {
+        setCurrentTextIndex((currentTextIndex + 1) % texts.length);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => setShowLogo(true), 500);
+    setTimeout(() => {
+      setShowMarquee(true);
+      animateText(texts[currentTextIndex].split(''));
+    }, 1000);
+  }, [currentTextIndex]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+    <Head>
+      <meta name="description" content="EBIV Clothing is an innovative venture poised to disrupt Chennai's streetwear scene. We leverage the distinctive properties of terry cotton to craft a line that resonates with global fashion trends, yet remains deeply rooted in the local culture. Our focus on customization and quality at affordable prices uniquely positions us in a rapidly evolving market."/>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta property='og:image' content="/x.png"/>
+      <link rel='icon' href='/l100px.png' />
+      <title>Ebiv Clothing</title>
+    </Head>
+    <div className="overflow-hidden w-full h-screen relative">
+      <motion.div
+        className="bg-[url('/bg.png')] bg-cover bg-no-repeat w-full h-screen flex items-center justify-center z-10"
+        initial={{ scale: 1.2 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+        style={{ position: 'relative' }}
+      >
+        {showMarquee && (
+          <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+            <div className="text-opacity-80 font-bold font-hj text-6xl text-black whitespace-nowrap animate-marquee">
+              {'COMING SOON ·   '.repeat(1000)}
+            </div>
+          </div>
+        )}
+        {showLogo && (
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="text-center"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <motion.div
+              animate={{ y: ["0%", "-5%", "0%"] }}
+              transition={{ repeat: Infinity, repeatType: "reverse", duration: 1 }}
+            >
+              <Image 
+                src="/logo.png" 
+                alt="Brand Logo" 
+                width={900} 
+                height={300}
+                layout="intrinsic"
+              />
+            </motion.div>
+            <h1 className='text-[80px] font-na  text-red-600'>
+              {currentText.map((letter, index) => (
+                <AnimatedLetter key={index} letter={letter} delay={index * 0.1} />
+              ))}
+            </h1>
+          </motion.div>
+        )}
+      </motion.div>
+      <style jsx global>{`
+        @keyframes marquee {
+          0% { transform: translateX(0); }
+          100% { transform: translateX(-80%); }
+        }
+        .animate-marquee {
+          animation: marquee 10s linear infinite;
+        }
+      `}</style>
+    </div>
+    </>
   );
 }
